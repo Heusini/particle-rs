cfe95819ca8d52585badd8f95a6e5357acd0ce48
Sebastian Heusinger <heusings@googlemail.com>
2021-06-15 17:07:59 +0200

    changed to send receive example

--- argon/examples/spi.rs
+++ argon/examples/spi.rs
@@ -4,8 +4,12 @@
 extern crate embedded_hal_spy;
 extern crate panic_halt;
 
+use embedded_hal::blocking::spi::Transfer;
+
+use core::fmt::Write;
 use cortex_m_rt::entry;
 use nrf52840_hal::gpio::Level;
+use nrf52840_hal::uarte;
 use nrf52840_hal::Delay;
 use nrf52840_hal::Spim;
 use particle_argon::{prelude::*, Board};
@@ -17,33 +21,57 @@ fn main() -> ! {
     // chipselect not needed here but needed for spi.transfer() functions
     //let cs = board.pins.a5.into_push_pull_output(Level::Low);
 
+    let pins = uarte::Pins {
+        txd: board.pins.tx.into_push_pull_output(Level::Low).degrade(),
+        rxd: board.pins.rx.into_floating_input().degrade(),
+        cts: None,
+        rts: None,
+    };
+
+    let mut uart = uarte::Uarte::new(
+        board.UARTE0,
+        pins,
+        uarte::Parity::EXCLUDED,
+        uarte::Baudrate::BAUD115200,
+    );
+
     let pins = nrf52840_hal::spim::Pins {
         sck: board.pins.sck.into_push_pull_output(Level::Low).degrade(),
         miso: Some(board.pins.miso.degrade()),
         mosi: Some(board.pins.mosi.into_push_pull_output(Level::Low).degrade()),
     };
 
-    let spi = Spim::new(
+    let mut spi = Spim::new(
         board.SPIM1,
         pins,
-        nrf52840_hal::spim::Frequency::M2,
+        nrf52840_hal::spim::Frequency::K125,
         nrf52840_hal::spim::MODE_0,
         0,
     );
 
-    let reference_data = b"Hello World!\n";
+    let message = b"Hello Welt!";
+
+    let mut tx_buffer = *message;
+    let mut rx_buffer = [0u8; 256];
+    // let reference_data = "Hello World!\n";
 
     let mut delay = Delay::new(board.SYST);
     let mut led = board.pins.d7.into_push_pull_output(Level::Low);
 
-    let mut eh_spi = embedded_hal_spy::new(spi, |_| {});
-    use embedded_hal::blocking::spi::Write;
+    // let mut eh_spi = embedded_hal_spy::new(spi, |_| {});
+    // use embedded_hal::blocking::spi::Write;
+    uart.write_str("hello\r\n").unwrap();
+
+    let mut cs = board.pins.a5.into_push_pull_output(Level::Low).degrade();
 
     loop {
-        match eh_spi.write(reference_data) {
-            Ok(_) => led.set_high().unwrap(),
-            Err(_) => led.set_low().unwrap(),
+        match spi.transfer_split_uneven(&mut cs, &mut tx_buffer, &mut rx_buffer) {
+            Ok(_) => {
+                uart.write(&rx_buffer).unwrap();
+                uart.write_str("\r\n");
+            }
+            Err(_) => led.set_high().unwrap(),
         }
-        delay.delay_ms(100_u32);
+        // cs.set_high();
     }
 }
